38228acf32a6e2534bbe229aedb1a28401b429e0
PINK FISH - STAVANGER LUFTHAVN <avd1003@salmoncompany.com>
2020-09-27 10:25:35 +0200

    Formularz

--- anagular-forms/src/app/app.component.ts
+++ anagular-forms/src/app/app.component.ts
@@ -1,10 +1,16 @@
 import { Component } from '@angular/core';
-
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
-  title = 'anagular-forms';
+  title = 'SDA-angular-forms';
+  user: { firstName: String; password: String } = {
+    firstName: '',
+    password: '',
+  };
+  onSubmit(): void {
+    console.log(this.user);
+  }
 }
